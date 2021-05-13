056991bc8e79e281f44dc03e5e90c4ecbc73a2e8
victoriaea97 <65075838+victoriaea97@users.noreply.github.com>
2021-05-13 22:37:18 +0100

    Updated RNN model

--- RNN_model.py
+++ RNN_model.py
@@ -1,11 +1,8 @@
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, LSTM, Dense, Embedding, concatenate, Dropout, concatenate
-from tensorflow.keras.layers import Bidirectional
-from sklearn.metrics import accuracy_score, f1_score
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Input, LSTM, Dense, Embedding, concatenate, Dropout, concatenate, Bidirectional
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.metrics import accuracy_score, f1_score
 from TextToTensor import TextToTensor
 from embeddings_matrix import *
 
@@ -18,9 +15,8 @@ class RNN():
         
         inp1 = Input(shape=(max_len,))
         x = Embedding(embedding_matrix.shape[0], embedding_dim, weights=[embedding_matrix])(inp1)
-        x = Bidirectional(LSTM(256, return_sequences=True))(x)
-        x = Bidirectional(LSTM(150))(x)
-        x = Dense(128, activation="relu")(x)
+        x = LSTM(256, return_sequences=True)(x)
+        x = LSTM(128)(x)
         x = Dropout(0.1)(x)
         x = Dense(64, activation="relu")(x)
         x = Dense(1, activation="sigmoid")(x)    
@@ -29,36 +25,34 @@ class RNN():
         model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
         self.model = model
 
-
-
 class Pipeline:
     """
     A class for the machine learning pipeline
     """
-    def __init__(self, X_train: list, Y_train: list, embed_path: str, embed_dim: int,
-        X_test=[], Y_test=[],epochs=3,batch_size=256):
+    def __init__(self, X_train: list, Y_train: list, embed_path: str, embed_dim: int,X_test=[], Y_test=[],max_len=None,epochs=3,batch_size=256):
 
-        # Preprocecing the text
-        #X_train = [clean_text(text, stop_words=stop_words) for text in X_train]
         Y_train = np.asarray(Y_train)
-        Y_test = np.asarray(Y_test)
         
         # Tokenizing the text
         tokenizer = Tokenizer()
         tokenizer.fit_on_texts(X_train)
+
+        # Saving the tokenizer
+        self.tokenizer = tokenizer
 
         # Creating the embedding matrix
         embedding = Embeddings(embed_path, embed_dim)
         embedding_matrix = embedding.create_embedding_matrix(tokenizer, len(tokenizer.word_counts))
 
         # Creating the padded input for the deep learning model
-        max_len = np.max([len(text.split()) for text in X_train])
+        if max_len is None:
+            max_len = np.max([len(text.split()) for text in X_train])
         TextToTensor_instance = TextToTensor(tokenizer=tokenizer, max_len=max_len)
         X_train = TextToTensor_instance.string_to_tensor(X_train)
 
         # Creating the model
         rnn = RNN(embedding_matrix=embedding_matrix, embedding_dim=embed_dim, max_len=max_len)
-        rnn.model.fit(X_train,Y_train, batch_size=batch_size, epochs=epochs, validation_data = (X_test, Y_test))
+        rnn.model.fit(X_train,Y_train, batch_size=batch_size, epochs=epochs)
 
         self.model = rnn.model
 
--- run_RNN.py
+++ run_RNN.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+import os
+import yaml
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import accuracy_score, f1_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -10,50 +12,66 @@ from TextToTensor import TextToTensor
 from RNN_model import * 
 from embeddings_matrix import *
 
-data1 = pd.DataFrame()
-data1 = pd.read_csv('Datasets/Train/TrainSample.csv', encoding = 'utf-8')
+# Reading the configuration file
 
-data2 = pd.DataFrame()
-data2 = pd.read_csv('Datasets/Test/TestSample.csv', encoding = 'utf-8')
+with open("conf.yml", 'r') as file:
+    conf = yaml.safe_load(file).get('pipeline')
 
-X_train, X_validation, y_train, y_validation = train_test_split(data1.Tweet, data1.Sentiment, test_size=0.35, random_state=4)
-
+# Getting the embedding
 embed_path = 'Embeddings/prac_embedding.txt'
-embed_dim = 100 # must be same size as the embedding dimension 
+embed_dim = 100 # must be same size as the embedding dimension
 
-# Tokenizing the text
-tokenizer = Tokenizer()
-tokenizer.fit_on_texts(X_train)
-num_unique_words = len(tokenizer.word_index)
+# Reading the data
+train = pd.read_csv('Datasets/Train/Train.csv', encoding = 'utf-8')[['Tweet', 'Sentiment']]
+test = pd.read_csv('Datasets/Test/Test.csv', encoding = 'utf-8')
 
-# Creating the embedding matrix
-embedding = Embeddings(embed_path, embed_dim)
-embedding_matrix = embedding.create_embedding_matrix(tokenizer, len(tokenizer.word_counts))
+# Shuffling the data for the k fold analysis
+train = train.sample(frac=1)
 
-# Getting the biggest sentence
-max_len = np.max([len(text.split()) for text in X_train])
+# Creating the input for the pipeline
+X_train = train['Tweet'].tolist()
+Y_train = train['Sentiment'].tolist()
 
-# Converting to tensor
-TextToTensor_instance = TextToTensor(tokenizer=tokenizer,max_len=max_len)
-X_train_NN = TextToTensor_instance.string_to_tensor(X_train)
+X_test = test['Tweet'].tolist()
+
+if conf.get('k_fold'):
+    kfold = KFold(n_splits=5)
+    acc = []
+    f1 = []
+    for train_index, test_index in kfold.split(X_train):
+        # Fitting the model and forecasting with a subset of data
+        k_results = Pipeline(
+            X_train=np.array(X_train)[train_index],
+            Y_train=np.array(Y_train)[train_index], 
+            embed_path=embed_path,
+            embed_dim=embed_dim,
+            X_test=np.array(X_train)[test_index],
+            Y_test=np.array(Y_train)[test_index],
+            max_len=conf.get('max_len'),
+            epochs=conf.get('epochs'),
+            batch_size=conf.get('batch_size')
+        )
+        # Saving the accuracy
+        acc += [k_results.acc]
+        f1 += [k_results.f1]
+        print(f'The accuracy score is: {acc[-1]}') 
+        print(f'The f1 score is: {f1[-1]}') 
+    print(f'Total mean accuracy is: {np.mean(acc)}')
+    print(f'Total mean f1 score is: {np.mean(f1)}')
+
+# Running the pipeline with all the data
+results = Pipeline(X_train=X_train,Y_train=Y_train, embed_path=embed_path,embed_dim=embed_dim,X_test=X_test,max_len=conf.get('max_len'),epochs=conf.get('epochs'),batch_size=conf.get('batch_size'))
 
 
-model = Sequential()
-model.add(Embedding(input_dim=num_unique_words+1, output_dim=embed_dim, input_length=max_len,weights=[embedding_matrix]))
 
-model.compile('rmsprop', 'mse')
-output_array = model.predict(X_train_NN)[0]
+# Saving the predictions
+test['prob_is_positive'] = results.yhat
+test['Prediction'] = [1 if x > 0.5 else -1 for x in results.yhat]
+ 
+# Saving the predictions to a csv file
+if conf.get('save_results'):
+    if not os.path.isdir('output'):
+        os.mkdir('output')    
+    test[['Prediction']].to_csv(f'TestPredictions.csv', index=False)
 
-results = Pipeline( X_train=X_train, Y_train=y_train, embed_path=embed_path, embed_dim=embed_dim, X_test=X_validation, Y_test= y_validation, epochs=10, batch_size=256)
-
-#save the model weights
-results.model.save("model.h5")
-
-
-# initialising the test data
-X_test = data2.loc[:,'Tweet']
-y_test = data2.loc[:,'Sentiment']
-
-X_test_nn = TextToTensor_instance.string_to_tensor(X_test)
-predictions = results.model.predict_classes(X_test_nn)
 
