986c382a2078a91068b41de5b2e16f36b5bd3d24
Neavewray1 <74053620+Neavewray1@users.noreply.github.com>
2021-05-19 15:13:51 +0100

    Updated Random Forest

--- RandomForest.py
+++ RandomForest.py
@@ -1,26 +1,30 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 from sklearn.model_selection import train_test_split
 
 dataset = pd.read_csv('cleaned_tweets.csv', encoding = 'utf-8')
-# split into train test sets
-train, test = train_test_split(dataset, test_size=0.2)
+
+print(dataset.describe())
+dataset_dp = dataset.dropna()
 
 
-x_train = train.loc[:,'Tweet']
+train, test = train_test_split(dataset_dp, test_size=0.2)
+
+
+x_train = train.loc[:,'SpellCheckTweets']
 y_train = train.loc[:,'Sentiment']
-x_test = test.loc[:,'Tweet']
+x_test = test.loc[:,'SpellCheckTweets']
 y_test = test.loc[:,'Sentiment']
 
 
-count_vectorizor = CountVectorizer(max_features=100)
+count_vectorizor = CountVectorizer(max_features=500)
 x_train = count_vectorizor.fit_transform(x_train).toarray()
 x_test = count_vectorizor.transform(x_test).toarray()
 
-clf = RandomForestClassifier(max_leaf_nodes=2)
+
+clf = RandomForestClassifier(n_estimators=50, max_leaf_nodes=50, max_depth=40)
 clf.fit(x_train, y_train)
 print("Training accuracy", clf.score(x_train, y_train))
 print("Test accuracy", clf.score(x_test, y_test))
