610cb4666b025dd2d85ae4aebcf2c34a0066ef6c
Neavewray1 <74053620+Neavewray1@users.noreply.github.com>
2021-05-19 15:12:57 +0100

    Updated Decision Tree

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,25 +1,31 @@
-import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+import pandas as pd
 
-data1 = pd.DataFrame()
-data1 = pd.read_csv('Datasets/Train/Train.csv', encoding = 'utf-8')
+dataset = pd.read_csv('cleaned_tweets.csv', encoding = 'utf-8')
 
-data2 = pd.DataFrame()
-data2 = pd.read_csv('Datasets/Test/Test.csv', encoding = 'utf-8')
-
-x_train = data1.loc[:,'Tweet']
-y_train = data1.loc[:,'Sentiment']
-x_test = data2.loc[:,'Tweet']
-y_test = data2.loc[:,'Sentiment']
+print(dataset.describe())
+dataset_dp = dataset.dropna()
 
 
-count_vectorizor = CountVectorizer(max_features=100)
+train, test = train_test_split(dataset_dp, test_size=0.2, random_state=42)
+
+
+x_train = train.loc[:,'SpellCheckTweets']
+y_train = train.loc[:,'Sentiment']
+x_test = test.loc[:,'SpellCheckTweets']
+y_test = test.loc[:,'Sentiment']
+
+
+count_vectorizor = CountVectorizer(max_features=500)
 x_train = count_vectorizor.fit_transform(x_train).toarray()
 x_test = count_vectorizor.transform(x_test).toarray()
 
-clf = DecisionTreeClassifier(max_leaf_nodes=2)
+
+clf = DecisionTreeClassifier(max_leaf_nodes=50)
 clf.fit(x_train, y_train)
 print("Training accuracy", clf.score(x_train, y_train))
 print("Test accuracy", clf.score(x_test, y_test))
+
+
