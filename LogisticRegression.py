3b94fff281a914f6cac8e21877b5213b9ec6b695
Vedang Joshi <50496437+vedang-joshi@users.noreply.github.com>
2021-05-21 12:01:49 +0100

    Added code for confusion matrix and fixed bugs

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,55 +1,139 @@
 import pandas as pd
+import pickle
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
-data1 = pd.DataFrame()
-data1 = pd.read_csv('Datasets/Train/Train.csv', encoding = 'utf-8')
 
-data2 = pd.DataFrame()
-data2 = pd.read_csv('Datasets/Test/Test.csv', encoding = 'utf-8')
+cleanedDf = pd.read_csv('dataset_cleaned_tweets.csv', usecols=['Sentiment', 'SpellCheckTweets'])
+#print(cleanedDf['Sentiment'].value_counts(normalize = True))
+cleanedDf = cleanedDf.dropna()
+X = cleanedDf['SpellCheckTweets']
+y = cleanedDf['Sentiment']
 
-
-#X_train, X_test, y_train, y_test = train_test_split(data.Tweet, data.Sentiment, test_size=0.35, random_state=4)
-x_train = data1.loc[:,'Tweet']
-y_train = data1.loc[:,'Sentiment']
-x_test = data2.loc[:,'Tweet']
-y_test = data2.loc[:,'Sentiment']
+#Splitting all data into 20% test and 80% training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=200)
 
 
-lr = LogisticRegression(penalty='l2', C=.8, random_state=21)
+lr = LogisticRegression(penalty='l2', C=1, solver='saga')
 text_classifier = Pipeline([('vectorizer', CountVectorizer(binary=False, stop_words='english', ngram_range=(1,2))), ('tfidf', TfidfTransformer(use_idf=True)),('clf', lr),])
-text_classifier.fit(x_train, y_train)
+model = text_classifier.fit(X_train, y_train)
+
+YPred = model.predict(X_test)
+
+##### Dump Classification report into pickle file
+pickle.dump(metrics.classification_report(y_test,YPred), open("classification_report_decision_tree.pkl", "wb"))
+confusion_matrix = pd.crosstab(y_test, YPred)
+plt.show()
+print()
+
+confusion_matrix = confusion_matrix.to_numpy()
+
+def make_confusion_matrix(cf,
+                          group_names=None,
+                          categories='auto',
+                          count=True,
+                          percent=True,
+                          cbar=True,
+                          xyticks=True,
+                          xyplotlabels=True,
+                          sum_stats=True,
+                          figsize=None,
+                          cmap='Blues',
+                          title=None):
+    '''
+    This function will make a pretty plot of an sklearn Confusion Matrix cm using a Seaborn heatmap visualization.
+    Arguments
+    ---------
+    cf:            confusion matrix to be passed in
+    group_names:   List of strings that represent the labels row by row to be shown in each square.
+    categories:    List of strings containing the categories to be displayed on the x,y axis. Default is 'auto'
+    count:         If True, show the raw number in the confusion matrix. Default is True.
+    normalize:     If True, show the proportions for each category. Default is True.
+    cbar:          If True, show the color bar. The cbar values are based off the values in the confusion matrix.
+                   Default is True.
+    xyticks:       If True, show x and y ticks. Default is True.
+    xyplotlabels:  If True, show 'True Label' and 'Predicted Label' on the figure. Default is True.
+    sum_stats:     If True, display summary statistics below the figure. Default is True.
+    figsize:       Tuple representing the figure size. Default will be the matplotlib rcParams value.
 
 
-# Obtaining the accuracy and confusion matrix when passing train set
-predicted_train = text_classifier.predict(x_train)
-predicted_proba_train = text_classifier.predict_proba(x_train)
-y_train = y_train.astype('category')
-print(metrics.classification_report(y_train, predicted_train,labels=y_train.cat.categories.tolist()))
-ConMatTrain=metrics.confusion_matrix(y_train, predicted_train)
-TrainAccuracy = (ConMatTrain[0,0] + ConMatTrain[1,1]) / (ConMatTrain[0,0] + ConMatTrain[1,1] + ConMatTrain[0,1] + ConMatTrain[1,0])
-print('TRAINING CONFUSION MATRIX:')
-print(ConMatTrain)
-print(f'Training Accuracy = {TrainAccuracy:.2f}')
+    title:         Title for the heatmap. Default is None.
+    '''
+
+    # CODE TO GENERATE TEXT INSIDE EACH SQUARE
+    blanks = ['' for i in range(cf.size)]
+
+    if group_names and len(group_names) == cf.size:
+        group_labels = ["{}\n".format(value) for value in group_names]
+    else:
+        group_labels = blanks
+
+    if count:
+        group_counts = ["{0:0.0f}\n".format(value) for value in cf.flatten()]
+    else:
+        group_counts = blanks
+
+    if percent:
+        group_percentages = ["{0:.2%}".format(value) for value in cf.flatten() / np.sum(cf)]
+    else:
+        group_percentages = blanks
+
+    box_labels = [f"{v1}{v2}{v3}".strip() for v1, v2, v3 in zip(group_labels, group_counts, group_percentages)]
+    box_labels = np.asarray(box_labels).reshape(cf.shape[0], cf.shape[1])
+
+    # CODE TO GENERATE SUMMARY STATISTICS & TEXT FOR SUMMARY STATS
+    if sum_stats:
+        # Accuracy is sum of diagonal divided by total observations
+        accuracy = np.trace(cf) / float(np.sum(cf))
+
+        # if it is a binary confusion matrix, show some more stats
+        if len(cf) == 2:
+            # Metrics for Binary Confusion Matrices
+            precision = cf[1, 1] / sum(cf[:, 1])
+            recall = cf[1, 1] / sum(cf[1, :])
+            f1_score = 2 * precision * recall / (precision + recall)
+            stats_text = "\n\nAccuracy={:0.3f}\nPrecision={:0.3f}\nRecall={:0.3f}\nF1 Score={:0.3f}".format(
+                accuracy, precision, recall, f1_score)
+        else:
+            stats_text = "\n\nAccuracy={:0.3f}".format(accuracy)
+    else:
+        stats_text = ""
+
+    # SET FIGURE PARAMETERS ACCORDING TO OTHER ARGUMENTS
+    if figsize == None:
+        # Get default figure size if not set
+        figsize = plt.rcParams.get('figure.figsize')
+
+    if xyticks == False:
+        # Do not show categories if xyticks is False
+        categories = False
+
+    # MAKE THE HEATMAP VISUALIZATION
+    f = plt.figure(figsize=figsize)
+    sns.heatmap(cf, annot=box_labels, fmt="", cmap=cmap, cbar=cbar, xticklabels=categories, yticklabels=categories)
+
+    if xyplotlabels:
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label' + stats_text)
+    else:
+        plt.xlabel(stats_text)
+
+    if title:
+        plt.title(title)
+    f.savefig("Logistic_Regression_confusion_matrix.pdf", bbox_inches='tight')
 
 
-# Obtaining the accuracy and confusion matrix when passing test set
-predicted_test = text_classifier.predict(x_test)
-predicted_proba_test = text_classifier.predict_proba(x_test)
-y_test = y_test.astype('category')
-print(metrics.classification_report(y_test, predicted_test,labels=y_test.cat.categories.tolist()))
-ConMatTest = metrics.confusion_matrix(y_test, predicted_test)
-TestAccuracy = (ConMatTest[0,0] + ConMatTest[1,1]) / (ConMatTest[0,0] + ConMatTest[1,1] + ConMatTest[0,1] + ConMatTest[1,0])
-print('TESTING CONFUSION MATRIX:')
-print(ConMatTest)
-print(f'Test Accuracy = {TestAccuracy:.2f}')
-
-
-# Saving the predictions to a csv file
-#data2['Predictions'] = predicted_test
-#data2.to_csv('log_reg_preds.csv')
+labels = ['True Neg','False Pos','False Neg','True Pos']
+categories = ['Negative', 'Positive']
+make_confusion_matrix(confusion_matrix,
+                      group_names=labels,
+                      categories=categories,
+                      cmap='BuGn', title='Confusion Matrix: Logistic Rgression model')
